b96727368c37e1d04112e91359df9e0fd78dc625
lucas <lucas@phala.network>
2023-07-17 16:57:49 +0800

    tailwind for html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,tsx, ts}"],
+  content: ["./src/**/*.{js,tsx, ts}", "./public/**/*.html"],
   theme: {
     extend: {},
   },
